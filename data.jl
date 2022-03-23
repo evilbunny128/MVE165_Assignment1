9cf686a3512259b723370fdfb43bedb86028705b
Joel Wall <joel@wallhouse.se>
2022-03-23 16:03:24 +0100

    Crop use data

---
+++ data.jl
@@ -0,0 +1,8 @@
+
+
+Resource_Max = [1600, 5000] # [amount of land, amount of water]
+
+Liters_oil_per_Ha = [2600 * 0.178, 1400 * 0.216, 900 * 0.433] # Litres per hectar for 
+# [soybeans, sunflower seeds, cotton seeds]
+
+Litres_water_per_Ha = [5, 4.2, 1]

83f038e2bfab3af41d688cbffc4a8feb5c61ee52
Joel Wall <joel@wallhouse.se>
2022-03-23 16:33:53 +0100

    Model for land use part

--- data.jl
+++ data.jl
@@ -1,8 +1,12 @@
 
+Crops = ["Soybeans", "Sunflower seeds", "Cotton seeds"]
+I_land_use = 1:3
+J_land_use = 1:2 # set of constraints
 
 Resource_Max = [1600, 5000] # [amount of land, amount of water]
 
-Liters_oil_per_Ha = [2600 * 0.178, 1400 * 0.216, 900 * 0.433] # Litres per hectar for 
+Liters_oil_per_Ha = [2600 * 0.178 1400 * 0.216 900 * 0.433] # Litres per hectar for 
 # [soybeans, sunflower seeds, cotton seeds]
+Litres_water_per_Ha = [5 4.2 1]
 
-Litres_water_per_Ha = [5, 4.2, 1]
+Resource_use = [1 1 1; Litres_water_per_Ha]
--- model.jl
+++ model.jl
@@ -1,2 +1,9 @@
 
-
+function build_land_use_model(data_file::String)
+    include(data_file)
+    m = Model()
+    @variable(m, x[I] >= 0) # amount of land used for each crop.
+    @constraint(m, Resource_Max, Resource_use * x .<= Resource_Max)
+    @objective(m, Max, Liters_oil_per_Ha * x)
+    return m, x
+end
